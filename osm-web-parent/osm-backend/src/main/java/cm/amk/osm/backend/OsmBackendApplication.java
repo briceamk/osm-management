b94ac96a568021e245537966ed5939a1949179a2
Brice amk <ambiandji@gmail.com>
2021-08-01 02:39:02 +0100

    initial commit

---
+++ osm-web-parent/osm-backend/src/main/java/cm/amk/osm/backend/OsmBackendApplication.java
@@ -0,0 +1,13 @@
+package cm.amk.osm.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsmBackendApplication.class, args);
+    }
+
+}
---
+++ osm-web-parent/osm-frontend/src/main/java/cm/amk/osm/frontend/OsmFrontendApplication.java
@@ -0,0 +1,13 @@
+package cm.amk.osm.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsmFrontendApplication.class, args);
+    }
+
+}
